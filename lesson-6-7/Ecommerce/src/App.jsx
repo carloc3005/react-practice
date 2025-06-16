d34949efe472246aeec6d5597c2b2129277dcfe2
carloc3005 <carlo89512@yahoo.com>
2025-06-16 16:32:06 -0700

    fixed router

--- lesson-6-7/Ecommerce/src/App.jsx
+++ lesson-6-7/Ecommerce/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Remove BrowserRouter as Router
 import Homepage from './pages/HomePage/Homepage';
 import OrdersPage from './pages/OrdersPage/OrdersPage'; // Updated import path
 import CheckoutPage from './pages/CheckoutPage/CheckoutPage';
@@ -22,14 +22,14 @@ function App() {
 
 
   return (
-    <Router>
+    
       <Routes>
         <Route index element={<Homepage cart={cart}/>} />
         <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
         <Route path="orders" element={<OrdersPage cart={cart}/>} />
         <Route path="tracking" element={<TrackingPage />} />
       </Routes>
-    </Router>
+    
   )
 }
 
